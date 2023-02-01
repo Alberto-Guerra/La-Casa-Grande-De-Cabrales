54d32d650631515af71cc0b2437a244e9a2162db
Trogui <albertoguerrarodas@gmail.com>
2023-02-01 18:55:16 +0100

    Nav & Hero done

---
+++ la-casa-grande/components/Hero.tsx
@@ -0,0 +1,62 @@
+import {
+  Stack,
+  Flex,
+  Button,
+  Text,
+  VStack,
+  useBreakpointValue,
+} from "@chakra-ui/react";
+
+export default function WithBackgroundImage() {
+  return (
+    <Flex
+      fontFamily={"Lato"}
+      w={"full"}
+      h={"100vh"}
+      background={
+        "linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('https://lh3.googleusercontent.com/p/AF1QipOLbwxwXIhuqsKAO9mKMl2opWc457W3yFN-UAyk=s680-w680-h510');"
+      }
+      backgroundSize={"cover"}
+      backgroundPosition={"center center"}
+    >
+      <VStack
+        w={"full"}
+        justify={"center"}
+        px={useBreakpointValue({ base: 4, md: 8 })}
+      >
+        <Stack textAlign={"center"} maxW={"2xl"} spacing={6} filter={"drop-shadow(5px 5px 5px black)"}>
+          <Text
+            color={"white"}
+            fontWeight={700}
+            lineHeight={1.2}
+            fontSize={useBreakpointValue({ base: "3xl", md: "5xl" })}
+            
+          >
+            La Casa Grande de Cabrales
+          </Text>
+          <Text
+            color={"white"}
+            fontWeight={400}
+            lineHeight={1.2}
+            fontSize={useBreakpointValue({ base: "2xl", md: "4xl" })}
+          >
+            En el corazón de los picos de europa
+          </Text>
+          <Stack >
+            <Button alignSelf={"center"}
+              bg={"#192E23"}
+              rounded={"full"}
+              width={"30%"}
+              color={"white"}
+              height={useBreakpointValue({ base: "30px", md: "50px" })}
+              fontSize={useBreakpointValue({ base: "md", md: "2xl" })}
+              _hover={{ bg: "#234231" }}
+            >
+                Ver Casas
+            </Button>
+          </Stack>
+        </Stack>
+      </VStack>
+    </Flex>
+  );
+}
